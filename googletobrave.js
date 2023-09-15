617aa8946b5bddc9a0a0ecb4be283a0dcaec9223
Dariel Quintanilla <darielquinta@gmail.com>
2023-09-14 22:26:47 -0400

    Update googletobrave.js

--- googletobrave.js
+++ googletobrave.js
@@ -1,24 +1,37 @@
 // ==UserScript==
-// @name         Redirect Google to Brave Search
+// @name         Intercept Google to Brave Search
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Redirects Google search queries to Brave search
 // @author       You
-// @match        *://www.google.com/search?*
+// @match        *://www.google.com/*
 // @grant        none
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    // Get the search query from the URL
-    let params = new URLSearchParams(window.location.search);
-    let query = params.get("q");
+    function interceptSearch(e) {
+        // Prevent Google from doing its thing
+        e.preventDefault();
 
-    // Construct the Brave search URL
-    let braveSearchUrl = `https://search.brave.com/search?q=${query}`;
+        // Get the search query
+        let query = document.querySelector('input[name="q"]').value;
 
-    // Redirect to Brave search
-    window.location.href = braveSearchUrl;
+        // Construct the Brave search URL
+        let braveSearchUrl = `https://search.brave.com/search?q=${query}`;
+
+        // Redirect to Brave search
+        window.location.href = braveSearchUrl;
+    }
+
+    // Wait for the search form to be available
+    let checkExist = setInterval(function() {
+        let form = document.querySelector('form[name="f"]');
+        if (form !== null) {
+            form.addEventListener('submit', interceptSearch);
+            clearInterval(checkExist);
+        }
+    }, 100); // check every 100ms
 })();
