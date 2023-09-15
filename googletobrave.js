06f6e25859c4dd80b1adef9e6a81bdb19511ff04
Dariel Quintanilla <darielquinta@gmail.com>
2023-09-14 22:21:51 -0400

    Create googletobrave.js

---
+++ googletobrave.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Redirect Google to Brave Search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Redirects Google search queries to Brave search
+// @author       You
+// @match        *://www.google.com/search?*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Get the search query from the URL
+    let params = new URLSearchParams(window.location.search);
+    let query = params.get("q");
+
+    // Construct the Brave search URL
+    let braveSearchUrl = `https://search.brave.com/search?q=${query}`;
+
+    // Redirect to Brave search
+    window.location.href = braveSearchUrl;
+})();
