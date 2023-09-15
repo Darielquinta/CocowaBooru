45cc2e6a3b676e34b9476af83d10ab111eaff43f
Dariel Quintanilla <darielquinta@gmail.com>
2023-09-14 22:28:52 -0400

    Update googletobrave.js

--- googletobrave.js
+++ googletobrave.js
@@ -12,26 +12,13 @@
 (function() {
     'use strict';
 
-    function interceptSearch(e) {
-        // Prevent Google from doing its thing
-        e.preventDefault();
+    // Get the search query from the URL
+    let params = new URLSearchParams(window.location.search);
+    let query = params.get("q");
 
-        // Get the search query
-        let query = document.querySelector('input[name="q"]').value;
+    // Construct the Brave search URL
+    let braveSearchUrl = `https://search.brave.com/search?q=${query}`;
 
-        // Construct the Brave search URL
-        let braveSearchUrl = `https://search.brave.com/search?q=${query}`;
-
-        // Redirect to Brave search
-        window.location.href = braveSearchUrl;
-    }
-
-    // Wait for the search form to be available
-    let checkExist = setInterval(function() {
-        let form = document.querySelector('form[name="f"]');
-        if (form !== null) {
-            form.addEventListener('submit', interceptSearch);
-            clearInterval(checkExist);
-        }
-    }, 100); // check every 100ms
+    // Redirect to Brave search
+    window.location.href = braveSearchUrl;
 })();
